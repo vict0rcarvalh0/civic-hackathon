1ffda16b3db996242bf1646f0b462cb39d44b94f
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2025-07-16 06:23:01 -0300

    refactor: navbar

--- src/skillpass/components/navigation.tsx
+++ src/skillpass/components/navigation.tsx
@@ -51,12 +51,6 @@ export default function Navigation() {
               >
                 Dashboard
               </Link>
-              <Link
-                href="/demo"
-                className="text-gray-300 hover:text-white transition-colors font-medium"
-              >
-                Web3 Demo
-              </Link>
               <ChainSelector />
               <AuthButton />
             </div>
